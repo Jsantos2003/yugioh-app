c8afeb9f0d5a24eadd82b8791f2db64d2fd66d27
Julio Cesar Santos Castillo <j.csc@hotmail.com>
2026-08-05 20:09:41 -0400

    Cambia el logo del navbar por el favicon personalizado

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import CartWidget from "@/components/CartWidget";
 import SearchBar from "@/components/SearchBar";
 
@@ -15,12 +16,13 @@ export default function Navbar() {
     <header className="navbar">
       <div className="navbar-inner">
         <Link href="/" className="brand">
-          <svg className="brand-icon" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <circle cx="32" cy="32" r="30" fill="none" stroke="#e2b23c" strokeWidth="2" />
-            <path d="M12 32c6-14 34-14 40 0-6 14-34 14-40 0z" fill="none" stroke="#e2b23c" strokeWidth="2" />
-            <circle cx="32" cy="32" r="8" fill="#e2b23c" />
-            <circle cx="32" cy="32" r="3.2" fill="#0b0b12" />
-          </svg>
+          <Image
+            src="/favicon.ico"
+            alt="Duel Hub"
+            width={30}
+            height={30}
+            className="brand-icon"
+          />
           <span className="brand-text">Duel Hub</span>
         </Link>
 
